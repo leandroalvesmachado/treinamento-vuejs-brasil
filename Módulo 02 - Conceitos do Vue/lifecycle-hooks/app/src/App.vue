186a918ad10c442c475bdebc6e3334a5539c28ae
Leandro Machado <leandroalvesmachado@gmail.com>
2022-10-05 09:47:35 -0300

    fim da aula Life-cycle hooks

---
+++ Módulo 02 - Conceitos do Vue/lifecycle-hooks/app/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <h1>Lifecycle hooks</h1>
+</template>
+
+<script>
+// import da composition API
+import { onMounted } from '@vue/runtime-core'
+
+  // options API
+  // export default {
+  //   mounted() {
+  //     console.log('Foi montado options API');
+  //   }
+  // }
+
+  // Composition API
+  export default {
+    setup() {
+      onMounted(() => {
+        console.log('Foi montado composition API')
+      })
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
