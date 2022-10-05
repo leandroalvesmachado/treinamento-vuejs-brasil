0b02e202158dd3f402b4aa7e33acb584ff2622a3
leandroalvesmachado@gmail.com <leandroalvesmachado@gmail.com>
2022-10-04 22:57:11 -0300

    salvando

---
+++ Módulo 02: Conceitos do Vue/data-binding/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <!-- v-bind:style = :style -->
+    <h1 :style="{ textDecoration: decoration }">Hello {{ name }}!</h1>
+    <input type="text" v-model="name">
+    <br>
+    <a :href="link">Link pro curso!</a>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    name: 'Igor',
+    link: 'https://treinamento.vuejsbrasil.org',
+    decoration: 'underline'
+  })
+}
+</script>
+
+<style>
+
+</style>
